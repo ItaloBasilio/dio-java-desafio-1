513dedd9b70b3a96367c438d67d88e2236b0d023
Italo Basilio da Silva <italobasilio@gmail.com>
2024-04-27 07:35:17 -0300

    update

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, Mundo!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.next();
+
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Por favor, digite o seu nome e sobrenome:");
+        String nomeSobrenome = scanner.nextLine();
+
+        System.out.println("Por favor, qual o valor do seu depósito ?");
+        Double deposito = scanner.nextDouble();
+
+        System.out.println("Olá, " + nomeSobrenome + ", obrigado por criar uma conta em nosso banco. Sua agência é: " + agencia + ", conta: " + conta + " e seu saldo de " + deposito + " já está disponível para saque.");
     }
 }
